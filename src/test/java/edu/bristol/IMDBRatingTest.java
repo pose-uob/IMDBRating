3d7b18040464e6d7190db4bdf8f4f82e7bb6b0c7
Ethan <ethanc-h@mail.com>
2022-02-21 12:27:38 +0000

    update 1

--- src/test/java/edu/bristol/IMDBRatingTest.java
+++ src/test/java/edu/bristol/IMDBRatingTest.java
@@ -16,5 +16,7 @@ public class IMDBRatingTest
 
         averageRating = rater.addNewRating(4);
         assertTrue(averageRating == 3.0, "Adding 2nd rating: average should be 3.0");
+        averageRating = rater.addNewRating(-3);
+        assertTrue(averageRating == 1.0);
     }
 }
