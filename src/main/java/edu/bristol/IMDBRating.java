bdcdada464d657d1f71256342bb4756cd9f621dc
evan <songevan666@gmail.com>
2022-02-22 19:14:52 +0000

    change feature

--- src/main/java/edu/bristol/IMDBRating.java
+++ src/main/java/edu/bristol/IMDBRating.java
@@ -1,16 +1,15 @@
 package edu.bristol;
 
-public class IMDBRating
-{
+public class IMDBRating {
     private float currentAverage = 0;
     private int ratingCount = 0;
 
-    public float addNewRating(int newRating)
-    {
-        int previousTotal = (int) (currentAverage * ratingCount);
-        int newTotal = previousTotal + newRating;
+    public float addNewRating(int newRating) {
+        float previousTotal = currentAverage * ratingCount;
+        float newTotal = previousTotal + newRating;
         ratingCount++;
         currentAverage = newTotal / ratingCount;
+        currentAverage = Math.round(currentAverage * 10.0) / 10.0f;
         return currentAverage;
     }
 }
