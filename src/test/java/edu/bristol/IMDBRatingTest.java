f6e1a1bfef24e19a603395586fa78e867b1f94c1
lywangj <og21893@bristol.ac.uk>
2022-02-23 00:21:16 +0000

    test5

--- src/test/java/edu/bristol/IMDBRatingTest.java
+++ src/test/java/edu/bristol/IMDBRatingTest.java
@@ -15,12 +15,15 @@ public class IMDBRatingTest
         assertTrue(averageRating == 2.0, "Adding 1st rating: average should be 2.0");
 
         averageRating = rater.addNewRating(4);
-        assertTrue(averageRating == 3.0, "Adding 2nd rating: average should be 3.0");
+        assertTrue(averageRating == 3, "Adding 2nd rating: average should be 3.0");
 
         averageRating = rater.addNewRating(0);
         assertTrue(averageRating == 2.0, "Adding 2nd rating: average should be 3.0");
 
         averageRating = rater.addNewRating(10);
+        assertTrue(averageRating == 4.0, "Adding 2nd rating: average should be 3.0");
+
+        averageRating = rater.addNewRating(4);
         assertTrue(averageRating == 4.0, "Adding 2nd rating: average should be 3.0");
     }
 }
